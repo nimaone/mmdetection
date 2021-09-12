f7b35407348c001c804685c267bdaec8473e588e
nimaone <58114984+nimaone@users.noreply.github.com>
2021-09-13 00:53:18 +0430

    Update transforms.py

--- mmdet/core/bbox/transforms.py
+++ mmdet/core/bbox/transforms.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
 import torch
+import cv2
 
 
 def bbox_flip(bboxes, img_shape, direction='horizontal'):
@@ -245,3 +246,141 @@ def bbox_xyxy_to_cxcywh(bbox):
     x1, y1, x2, y2 = bbox.split((1, 1, 1, 1), dim=-1)
     bbox_new = [(x1 + x2) / 2, (y1 + y2) / 2, (x2 - x1), (y2 - y1)]
     return torch.cat(bbox_new, dim=-1)
+def cal_line_length(point1, point2):
+    return math.sqrt( math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2))
+def TuplePoly2Poly(poly):
+    outpoly = [poly[0][0], poly[0][1],
+                       poly[1][0], poly[1][1],
+                       poly[2][0], poly[2][1],
+                       poly[3][0], poly[3][1]
+                       ]
+    return outpoly
+def get_best_begin_point_single(coordinate):
+    x1 = coordinate[0][0]
+    y1 = coordinate[0][1]
+    x2 = coordinate[1][0]
+    y2 = coordinate[1][1]
+    x3 = coordinate[2][0]
+    y3 = coordinate[2][1]
+    x4 = coordinate[3][0]
+    y4 = coordinate[3][1]
+    xmin = min(x1, x2, x3, x4)
+    ymin = min(y1, y2, y3, y4)
+    xmax = max(x1, x2, x3, x4)
+    ymax = max(y1, y2, y3, y4)
+    combinate = [[[x1, y1], [x2, y2], [x3, y3], [x4, y4]], [[x2, y2], [x3, y3], [x4, y4], [x1, y1]],
+                 [[x3, y3], [x4, y4], [x1, y1], [x2, y2]], [[x4, y4], [x1, y1], [x2, y2], [x3, y3]]]
+    dst_coordinate = [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
+    force = 100000000.0
+    force_flag = 0
+    for i in range(4):
+        temp_force = cal_line_length(combinate[i][0], dst_coordinate[0]) + cal_line_length(combinate[i][1],
+                                                                                           dst_coordinate[
+                                                                                               1]) + cal_line_length(
+            combinate[i][2], dst_coordinate[2]) + cal_line_length(combinate[i][3], dst_coordinate[3])
+        if temp_force < force:
+            force = temp_force
+            force_flag = i
+    if force_flag != 0:
+        pass
+        # print("choose one direction!")
+    return  combinate[force_flag]
+
+def get_best_begin_point_warp_single(coordinate):
+
+    return TuplePoly2Poly(get_best_begin_point_single(coordinate))
+
+def get_best_begin_point(coordinate_list):
+    best_coordinate_list = map(get_best_begin_point_warp_single, coordinate_list)
+    # import pdb
+    # pdb.set_trace()
+    best_coordinate_list = np.stack(list(best_coordinate_list))
+
+    return best_coordinate_list
+
+def mask2poly_single(binary_mask):
+    """
+    :param binary_mask:
+    :return:
+    """
+    # try:
+    contours, hierarchy = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # contour_lens = np.array(list(map(len, contours)))
+    # max_id = contour_lens.argmax()
+    # max_contour = contours[max_id]
+    max_contour = max(contours, key=len)
+    rect = cv2.minAreaRect(max_contour)
+    poly = cv2.boxPoints(rect)
+        # poly = TuplePoly2Poly(poly)
+    # except:
+        # import pdb
+        # pdb.set_trace()
+    return poly
+
+def mask2poly(binary_mask_list):
+    polys = map(mask2poly_single, binary_mask_list)
+    # polys = np.stack(polys
+    return list(polys)
+
+
+
+def polygonToRotRectangle_batch(bbox, with_module=True):
+    """
+    :param bbox: The polygon stored in format [x1, y1, x2, y2, x3, y3, x4, y4]
+            shape [num_boxes, 8]
+    :return: Rotated Rectangle in format [cx, cy, w, h, theta]
+            shape [num_rot_recs, 5]
+    """
+    # print('bbox: ', bbox)
+    bbox = np.array(bbox,dtype=np.float32)
+    bbox = np.reshape(bbox,newshape=(-1, 2, 4),order='F')
+    # angle = math.atan2(-(bbox[0,1]-bbox[0,0]),bbox[1,1]-bbox[1,0])
+    # print('bbox: ', bbox)
+    angle = np.arctan2(-(bbox[:, 0,1]-bbox[:, 0,0]),bbox[:, 1,1]-bbox[:, 1,0])
+    # angle = np.arctan2(-(bbox[:, 0,1]-bbox[:, 0,0]),bbox[:, 1,1]-bbox[:, 1,0])
+    # center = [[0],[0]] ## shape [2, 1]
+    # print('angle: ', angle)
+    center = np.zeros((bbox.shape[0], 2, 1))
+    for i in range(4):
+        center[:, 0, 0] += bbox[:, 0,i]
+        center[:, 1, 0] += bbox[:, 1,i]
+
+    center = np.array(center,dtype=np.float32)/4.0
+
+    # R = np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]], dtype=np.float32)
+    R = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]], dtype=np.float32)
+
+    normalized = np.matmul(R.transpose((2, 1, 0)),bbox-center)
+
+
+    xmin = np.min(normalized[:, 0, :], axis=1)
+    # print('diff: ', (xmin - normalized[:, 0, 3]))
+    # assert sum((abs(xmin - normalized[:, 0, 3])) > eps) == 0
+    xmax = np.max(normalized[:, 0, :], axis=1)
+    # assert sum(abs(xmax - normalized[:, 0, 1]) > eps) == 0
+    # print('diff2: ', xmax - normalized[:, 0, 1])
+    ymin = np.min(normalized[:, 1, :], axis=1)
+    # assert sum(abs(ymin - normalized[:, 1, 3]) > eps) == 0
+    # print('diff3: ', ymin - normalized[:, 1, 3])
+    ymax = np.max(normalized[:, 1, :], axis=1)
+    # assert sum(abs(ymax - normalized[:, 1, 1]) > eps) == 0
+    # print('diff4: ', ymax - normalized[:, 1, 1])
+
+    w = xmax - xmin + 1
+    h = ymax - ymin + 1
+
+    w = w[:, np.newaxis]
+    h = h[:, np.newaxis]
+    # TODO: check it
+    if with_module:
+        angle = angle[:, np.newaxis] % ( 2 * np.pi)
+    else:
+        angle = angle[:, np.newaxis]
+    dboxes = np.concatenate((center[:, 0].astype(np.float), center[:, 1].astype(np.float), w, h, angle), axis=1)
+
+def gt_mask_bp_obbs(gt_masks, with_module=True):
+
+    # trans gt_masks to gt_obbs
+    gt_polys = mask2poly(gt_masks)
+    gt_bp_polys = get_best_begin_point(gt_polys)
+    gt_obbs = polygonToRotRectangle_batch(gt_bp_polys, with_module)
