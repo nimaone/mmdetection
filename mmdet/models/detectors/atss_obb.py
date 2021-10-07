0c5c45ab68841ce022980c0e38040789157bb643
nimaone <58114984+nimaone@users.noreply.github.com>
2021-10-07 04:08:15 +0330

    Update atss_obb.py

--- mmdet/models/detectors/atss_obb.py
+++ mmdet/models/detectors/atss_obb.py
@@ -1,1 +1,19 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from ..builder import DETECTORS
+from .single_stage import SingleStageDetector
 
+
+@DETECTORS.register_module()
+class ATSS_obb(SingleStageDetector):
+    """Implementation of `ATSS <https://arxiv.org/abs/1912.02424>`_."""
+
+    def __init__(self,
+                 backbone,
+                 neck,
+                 bbox_head,
+                 train_cfg=None,
+                 test_cfg=None,
+                 pretrained=None,
+                 init_cfg=None):
+        super(ATSS_obb, self).__init__(backbone, neck, bbox_head, train_cfg,
+                                   test_cfg, pretrained, init_cfg)
