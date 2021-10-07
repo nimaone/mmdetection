045e46a984c3f352b8d8679478b9448efd481d9b
nimaone <58114984+nimaone@users.noreply.github.com>
2021-10-08 02:22:33 +0330

    Update retinanet_r50_fpn_1x_dota.py

--- configs/retinanet/retinanet_r50_fpn_1x_dota.py
+++ configs/retinanet/retinanet_r50_fpn_1x_dota.py
@@ -148,7 +148,7 @@ log_level = 'INFO'
 load_from = '/content/mmdetection/checkpoint/retinanet_r50_fpn_2x_coco_20200131-fdb43119.pth'
 resume_from = None
 workflow = [('train', 1)]
-work_dir = '/content/tutorial_exps'
+work_dir = '/content/drive/MyDrive/tutorial_exps'
 seed = 0
 gpu_ids = range(0, 1)
 
